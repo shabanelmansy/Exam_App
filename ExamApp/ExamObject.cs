1db81579debca032612507e470d8e4c387b4d949
unknown <shaban_elmansy@yahoo.com>
2021-02-08 14:14:42 +0400

    test

---
+++ ExamApp/ExamObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamApp
+{
+    class ExamObject
+    {
+        public void saveExam(string msg)
+        {
+            if (msg == "close")
+                Application.Exit();
+        }
+    }
+}
